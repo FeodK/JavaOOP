254af699dbe809f2e20daca95758b62d33aff63c
Feodor <mr.fedor.kiselev@gmail.com>
2023-12-25 17:39:52 +0300

    Task2HW

--- Task2HW/Main.java
+++ Task2HW/Main.java
@@ -3,11 +3,11 @@ package Task2HW;
 public class Main {
     public static void main(String[] args) {
 
-        Human numan1 = new Human("Рустам");
-        Human numan2 = new Human("Фоат");
+        Human numan1 = new Human("Василий");
+        Human numan2 = new Human("Роман");
 
         Market market = new Market();
-        
+
         market.acceptToMarket(numan1);
         market.acceptToMarket(numan2);
 
